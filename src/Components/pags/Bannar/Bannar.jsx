bac662ffd22849ca849c4b318ee8e77a0d1b3fbc
kausar017 <mdkousarmia71@gmail.com>
2025-05-20 17:56:24 +0600

    Popular Categories working

--- src/Components/pags/Bannar/Bannar.jsx
+++ src/Components/pags/Bannar/Bannar.jsx
@@ -6,7 +6,7 @@ const Bannar = () => {
     return (
         <div className='w-full max-w-[1300px] mx-auto pt-6'>
             <Link to='https://play.google.com/store/apps/details?id=com.bdstall.bdstall_app' target='blank'>
-                <img src="../../../../src/assets/Screenshot 2025-05-20 161926.png" className='max-w-full object-cover' alt="" />
+                <img src="../../../../src/assets/Screenshot 2025-05-20 161926.png" className='w-full object-cover' alt="" />
             </Link>
             <Popular />
         </div>
--- src/Components/pags/Popular/Popular.jsx
+++ src/Components/pags/Popular/Popular.jsx
@@ -16,7 +16,7 @@ const Popular = () => {
             <div>
                 <h1 className='text-2xl font-bold'>Popular Categories</h1>
             </div>
-            <div className='pt-5 grid grid-cols-8 gap-2 items-center'>
+            <div className='pt-5 grid xl:grid-cols-10 lg:grid-cols-6 md:grid-cols-4 max-sm:grid-cols-3 px-2  gap-2 items-center justify-between'>
                 {catagory?.map((item, i) => (
                     <Link key={i} className="h-full">
                         <div className="card bg-base-100 space-x-1 shadow-sm h-full flex flex-col hover:border border-red-500">
@@ -24,11 +24,11 @@ const Popular = () => {
                                 <img
                                     src={item?.image}
                                     alt={item?.category}
-                                    className="rounded-xl w-full max-w-14"
+                                    className="rounded-xl w-full max-w-10"
                                 />
                             </figure>
                             <div className="card-body items-center text-center flex-grow flex flex-col justify-center">
-                                <h2 className="card-title">{item?.category}</h2>
+                                <h2 className="font-bold text-red-600">{item?.category}</h2>
                             </div>
                         </div>
                     </Link>
